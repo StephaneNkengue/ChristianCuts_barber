3ce7b7f820d9a8fdab6320e52ad3e23472e374df
StephaneNkengue <nkenguestephane0@gmail.com>
2025-02-11 22:27:03 -0500

    Création du projet

---
+++ src/app/components/header.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const header = () => {
+  return (
+    <header className="bg-[#2C2D2F] text-white py-4">
+      <div className="container mx-auto flex justify-evenly items-center px-6">
+        <div className="text-3xl font-bold flex items-center ">
+          <span className="text-white">CHRISTIAN</span>
+          <Image
+            src="/icons/Logo_Christian1.PNG"
+            width={80}
+            height={80}
+            alt="Picture of the author"
+          />
+          <span className="text-white">CUTZ</span>
+        </div>
+
+        <nav className="flex text-2xl mx-20 space-x-6 text-gray-400">
+          <Link
+            href="#"
+            className="text-orange-500 transition duration-1000 ease-in-out"
+          >
+            Home
+          </Link>
+          <Link
+            href="#"
+            className="hover:text-orange-500  transition duration-1000 ease-in-out"
+          >
+            Services
+          </Link>
+          <Link
+            href="#"
+            className="hover:text-orange-500  transition duration-1000 ease-in-out"
+          >
+            Contact
+          </Link>
+        </nav>
+
+        <button
+          type="button"
+          className="relative border border-orange-500 text-white px-4 py-2 rounded-md overflow-hidden group"
+        >
+          <span className="absolute inset-0 bg-orange-500 scale-x-0 origin-left transition-transform duration-1000 ease-in-out group-hover:scale-x-100"></span>
+          <strong className="relative z-10 text-2xl">Book Appointment</strong>
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default header;
---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+import Header from "./components/header";
+
+export default function RootLayout() {
+  return (
+    <html lang="en">
+      <body>
+        <Header />
+      </body>
+    </html>
+  );
+}
