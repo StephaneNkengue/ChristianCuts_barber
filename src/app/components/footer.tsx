4e365d7212e8a75651d03c67014e9122d85d90fe
StephaneNkengue <nkenguestephane0@gmail.com>
2025-02-12 19:15:42 -0500

    Footer fait

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,27 +5,23 @@ import { FaInstagram, FaFacebookF, FaXTwitter } from "react-icons/fa6";
 
 export default function footer() {
   return (
-    <footer className="bg-[#020202] text-white py-10 text-center">
-      <div className="flex flex-col items-center space-y-4">
+    <footer className="bg-[#0c0c0c] text-white py-5 text-center h-96">
+      <div className="flex flex-col items-center	content-between space-y-7">
         {/* Logo */}
         <Link href="/">
-          <div className="text-3xl font-bold flex items-center ">
+          <div className="text-4xl font-bold flex items-center ">
             <span className="text-white">CHRISTIAN</span>
             <Image
               src="/icons/Logo2.PNG"
-              width={80}
-              height={80}
+              width={90}
+              height={90}
               alt="Picture of the author"
             />
             <span className="text-white">CUTZ</span>
           </div>
         </Link>
-        <p className="text-gray-400">
-          SINCE <span className="font-semibold">2022</span>
-        </p>
-
-        {/* Navigation */}
-        <nav className="flex space-x-6 text-gray-400">
+        <span className="text-white text-2xl">SINCE 2022</span>
+        <nav className="flex text-2xl mx-20 space-x-6 text-gray-400">
           <Link
             href="/"
             className="text-orange-500 transition duration-1000 ease-in-out"
@@ -46,8 +42,7 @@ export default function footer() {
           </Link>
         </nav>
 
-        {/* Social Icons */}
-        <div className="flex space-x-6 text-gray-400 text-2xl">
+        <div className="flex space-x-6 text-gray-400 text-4xl">
           <a href="#" className="hover:text-white">
             <FaInstagram />
           </a>
@@ -60,7 +55,7 @@ export default function footer() {
         </div>
 
         {/* Copyright */}
-        <p className="text-gray-500 text-sm">
+        <p className="text-white text-sm">
           Réalisé par Stéphane Darryl Wamo Nkengue. © Copyright 2025 Christian
           Cutz. Tous droits réservé
         </p>
