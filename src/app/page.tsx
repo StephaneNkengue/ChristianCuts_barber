7441456fa77342bef6057f3861de06e99f73eacd
StephaneNkengue <nkenguestephane0@gmail.com>
2025-02-18 19:04:05 -0500

    Ajout de la page d'acceuil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,36 @@
 import React from "react";
+import Image from "next/image";
 
 export default function page() {
-  return <div>home</div>;
+  return (
+    <div className="relative w-full h-screen">
+      {/* Background Image */}
+      <Image
+        src="/images/image-background.jpg"
+        alt="Barber"
+        layout="fill"
+        objectFit="cover"
+        className="absolute inset-0 z-0"
+      />
+
+      <div className="absolute inset-0 bg-black/50"></div>
+
+      <div className="relative z-10 flex flex-col justify-start items-start ms-44 h-full pt-44 px-10 md:px-20 text-white">
+        <h3 className="text-3xl uppercase font-bold">We are here</h3>
+        <h1 className="text-8xl font-bold mt-2 uppercase">
+          Trim & Style <br />
+          <span className="text-orange-500">You</span>
+        </h1>
+        <p className="mt-4 max-w-lg text-gray-300">
+          At Trim & Style, we are dedicated to providing exceptional grooming
+          and styling services tailored to your unique preferences. Let’s go!
+        </p>
+
+        {/* Button */}
+        <button className="mt-6 px-6 py-3 border-2 border-orange-500 text-white font-semibold hover:bg-orange-500 transition-all">
+          Book Appointment
+        </button>
+      </div>
+    </div>
+  );
 }
