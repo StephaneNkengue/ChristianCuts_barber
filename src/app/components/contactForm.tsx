4fc5f02d1dbbf5ffa823fddc390bcd54f0a159a3
StephaneNkengue <nkenguestephane0@gmail.com>
2025-02-24 21:12:43 -0500

    Validation de mon envoi de mail avec mailgun

--- src/app/components/contactForm.tsx
+++ src/app/components/contactForm.tsx
@@ -36,7 +36,7 @@ export default function contactForm() {
     setResponseMessage("");
 
     try {
-      const res = await fetch("/api/sendEmail/route", {
+      const res = await fetch("/api/sendEmail", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
