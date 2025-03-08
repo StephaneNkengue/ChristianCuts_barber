5db9a8d4b2ea3d6791e1ac3cf2958681a998cfd7
StephaneNkengue <nkenguestephane0@gmail.com>
2025-03-07 20:30:19 -0500

    Suppression de toutes les références de cal.com

--- src/components/ui/Calendar.tsx
+++
@@ -1,8 +0,0 @@
-import { DayPicker } from "react-day-picker";
-import "react-day-picker/dist/style.css";
-
-import { DayPickerProps } from "react-day-picker";
-
-export function Calendar(props: DayPickerProps) {
-  return <DayPicker {...props} />;
-}
